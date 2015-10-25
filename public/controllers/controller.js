4682b6cb9fc6435b3058761b559e4a45c5093e48
unknown <pedronobre@NOT-GRT-IOT-01.great.local.ufc.br>
2015-10-24 21:18:36 -0300

    adicionado DELETE e PUT methods

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,6 +4,7 @@ myApp.controller('AppCtrl',function ($scope, $http){
     console.log("Estou no controller!");
 
 var refresh = function() {
+    //Requisição GET
     $http.get("/contactlist").success(function(response) {
     	console.log("Peguei os dados");
     	$scope.contactlist = response;
@@ -13,7 +14,7 @@ var refresh = function() {
 
 refresh();
 
-//Butão de adicionar
+//Butão de adicionar POST
 $scope.addContact = function () {
 console.log("Adicionado contato: " + $scope.contact);
     $http.post('/contactlist',$scope.contact).success(function(response) {
@@ -22,12 +23,36 @@ console.log("Adicionado contato: " + $scope.contact);
     });
 }
 
-$scope.rmvContact = function() {
-	console.log("Contato removido");
+//Butão de remover DELETE
+$scope.rmvContact = function(id) {
+	console.log("ID: "+id+" enviado");
+    $http.delete('/contactlist/' + id).success(function(response) {
+        console.log(response+" removido");
+        refresh();
+    });
 }
 
+//Butão de editar GET
+$scope.edtContact = function(id) {
+    console.log("Editar contato "+ id);
+    $http.get("/contactlist/"+id).success(function(response) {
+        console.log("Contato editado para: "+response);
+        $scope.contact = response;
+    });
+}
+
+//Butão de atualizar PUT
 $scope.updContact = function() {
-	console.log("Contato editado");
+	console.log($scope.contact._id);
+    $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response) {
+        console.log("Contato atualizado para: "+response);
+        refresh();
+    });
+}
+
+//Butão de Limpar
+$scope.dsltContact = function () {
+    $scope.contact = "";
 }
 
 });
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require('express');
-var app = express();
-
 var mongojs = require('mongojs');
-var db = mongojs('contactlist',['contactlist']);
-
 var bodyParser = require('body-parser');
+
+var app = express();
+var db = mongojs('contactlist',['contactlist']);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+//GETs
 app.get('/contactlist', function (req, res) {
 	console.log('GET Request recebido.');
-
     db.contactlist.find(function (err, docs) {
         console.log(docs);
         res.json(docs);
@@ -19,11 +18,47 @@ app.get('/contactlist', function (req, res) {
     });
 });
 
+app.get("/contactlist/:id", function (req, res) {
+    console.log('GET Request recebido.');
+    var id = req.params.id;
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+        console.log(doc);
+        res.json(doc);
+        console.log("GET Response respondido.");
+    });
+});
+
+//POSTs
 app.post('/contactlist', function (req, res) {
+    console.log('POST Request recebido.');
     console.log(req.body);
     db.contactlist.insert(req.body, function (err, doc) {
         res.json(doc);
+        console.log("POST Response respondido.");
     })
+});
+
+//DELETEs
+app.delete('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log("DELETE Request recebido: objectId - "+id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+        res.json(doc);
+        console.log("DELETE Response enviado.");
+    });
+
+});
+
+//PUTs
+app.put('/contactlist/:id', function (req, res) {
+     var id = req.params.id;
+     console.log("PUT Request recebido.");
+     db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+     update: {$set: {nome: req.body.nome, email: req.body.email, telefone: req.body.telefone}}, new: true}, function (err, doc) {
+         res.json(doc);
+         console.log("PUT Response enviado.");
+        });
+
 });
 
 app.listen(3000);
